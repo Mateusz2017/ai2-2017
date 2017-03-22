348ff07dee6b84554405ad034a9e9bc137efbda9
Mateusz Kata <Mateusz2017@gmail.com>
2017-03-22 08:40:20 +0100

    Update string2.py

--- string2.py
+++ string2.py
@@ -48,14 +48,13 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  a_middle = len(a) / 2
-  b_middle = len(b) / 2
+  a_middle = int(len(a) / 2)
+  b_middle = int(len(b) / 2)
   if len(a) % 2 == 1:
     a_middle = a_middle + 1
   if len(b) % 2 == 1:
     b_middle = b_middle + 1
-  ble = a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
-  return ble
+  return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
 
 
 
